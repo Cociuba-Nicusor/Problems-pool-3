98e73836256470ccc252c162f4036cdd03867896
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-11 15:34:59 +0200

    ty

---
+++ Pool3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pool3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculati suma elementelor unui vector de n numere citite de la tastatura. Rezultatul se va afisa pe ecran.
+            int n, i, s = 0;
+            Console.WriteLine("Introduceti lungimea vectorului:");
+            n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (i = 0; i < n; i++)
+                a[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elementele tabloului sunt:");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            for (i = 0; i < n; i++)
+                s = s + a[i];
+            Console.WriteLine($"Suma elementelor din tablou este:{s}.");
+            Console.ReadKey();
+        }
+    }
+}
