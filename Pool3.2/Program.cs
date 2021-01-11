644f65ba0a5e0f5422536efc263d8648bcbc62b5
Cociuba Nicusor <cociubanicusor66@gmail.com>
2021-01-11 16:05:52 +0200

    ty

---
+++ Pool3.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pool3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se da un vector cu n elemente si o valoare k. 
+            //Se cere sa se determine prima pozitie din vector pe care apare k. 
+            //Daca k nu apare in vector rezultatul va fi -1.
+            int n, i, k, z = 0, u = 0;
+            Console.WriteLine("Introduceti lungimea vectorului:");
+            n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for (i = 0; i < n; i++)
+                a[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea cautata:");
+            k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Elementele tabloului sunt:");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+           for (i = 0; i < n; i++)
+            {
+                if (a[i] == k)
+                    z = 1;
+                u = i;
+            }
+            if (z == 1)
+                Console.WriteLine($"Valoarea {k} se afla pe pozitia {u}.");
+            else
+                Console.WriteLine("-1");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pool3.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pool3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, i;
+            Console.Write("n: ");
+            n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            for ( i = 0; i < n; i++)
+            {
+               a[i] = int.Parse(Console.ReadLine());
+            }
+                int poz1 = 0;
+                int min = a[0];
+                int max = a[0];
+                int poz2 = 0;
+                for ( i = 0; i < n; i++)
+                {
+                    if (a[i] < min)
+                    {
+                        min = a[i];
+                        poz1 = i;
+                    }
+                    if (a[i] > max)
+                    {
+                        max = a[i];
+                        poz2 = i;
+                    }
+                }
+            Console.WriteLine($"Minimul se afla pe pozitia {poz1} \n Maximul se afla pe pozitia {poz2}");
+            Console.ReadKey();
+        }
+    }
+}
